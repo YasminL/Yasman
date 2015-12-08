491abb5364006d67047321524b9cffe4d3e06e24
Yasmin Lindholm <yasmin.lindholm@ffcg.se>
2015-12-08 20:29:25 +0100

    first commit

---
+++ Yasman/Yasman/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Yasman
+//
+//  Created by Yasmin on 08/12/15.
+//  Copyright © 2015 Yasmin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#define TAG @"ViewController"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonTapped:(id)sender {
+    NSLog(TAG);
+}
+
+@end
---
+++ Yasman/Yasman/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yasman
+//
+//  Created by Yasmin on 08/12/15.
+//  Copyright © 2015 Yasmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
